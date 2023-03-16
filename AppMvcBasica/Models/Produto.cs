bc3e430136651d6b178bb78bcf4ed53a6090f7c8
Douglas Leal <douglas.leal@live.com>
2023-03-16 10:32:22 -0300

    Migration dos modelos criado

--- AppMvcBasica/Models/Produto.cs
+++ AppMvcBasica/Models/Produto.cs
@@ -2,7 +2,7 @@
 
 namespace AppMvcBasica.Models
 {
-    public class Produto
+    public class Produto : Entity
     {
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
